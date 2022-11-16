c970f5016a8c5258b0b5be5a485cbb319433b248
JorginhoO <jorge.infoo88@gmail.com>
2022-11-15 21:03:52 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import NotificationButton from "./componets/NotificationButton"
 
 function App() {
   return (
-    <h1>Ola mundo</h1>
+    <>
+      <h1>Olá mundo</h1>
+      <NotificationButton />
+    </>
   )
 }
 
---
+++ frontend/src/componets/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+import icon from '../../assets/img/icon-notification.svg'
+import './style.css'
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-btn">
+            <img src={icon} alt="Notificar"/>
+        </div>
+    )
+}
+
+export default NotificationButton
