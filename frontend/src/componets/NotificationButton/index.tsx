2a3e207886abc25a4d99fb7abdc92b5a563634fc
JorginhoO <jorge.infoo88@gmail.com>
2022-11-18 23:16:06 -0300

    TOAST

--- frontend/src/componets/NotificationButton/index.tsx
+++ frontend/src/componets/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/icon-notification.svg';
 import { BASE_URL } from '../../utils/request';
 import './style.css';
@@ -10,8 +11,8 @@ type Props = {
 function handleClick(id: number) {
     axios(`${BASE_URL}/sales/${id}/notification`)
         .then(Response => {
-            console.log("SUCESSO")
-        })
+            toast.info("SMS enviado com sucesso");
+        });
 }
 
 
